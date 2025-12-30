b0fcba5f369bd6a5c623ce8dd1339be483bf9eb4
Simeon Golemdzhiev <simeongolemdzhiev2003@gmail.com>
2025-12-30 21:09:46 +0200

    new
    Signed-off-by: Simeon Golemdzhiev <simeongolemdzhiev2003@gmail.com>

--- Telegram News Bot/main.py
+++ Telegram News Bot/main.py
@@ -8,7 +8,6 @@ if __name__ == '__main__':
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("news", news_handler))
     app.add_handler(CommandHandler("price", price_handler))
-    app.add_handler(CommandHandler("gabi", gabi))
 
     print("Bot is activate")
     app.run_polling()
